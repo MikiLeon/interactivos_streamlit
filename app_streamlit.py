3bd336a34a57b8ea3d4287a7d68a74113e597fbd
MikiLeon <miiguelleon@gmail.com>
2025-03-24 17:42:19 +0100

    Update_code_app_streamlit.py
    actualización de codigo

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,17 +2,14 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import openpyxl
 
 
 pd.options.display.max_columns= None
 
-
 # Cargar el CSV (asegúrate de cambiar 'datos.csv' por el nombre de tu archivo)
-DATA = ("https://raw.githubusercontent.com/MikiLeon/interactivos_streamlit/refs/heads/main/mapa.csv")
+DATA = ('C:\\Users\\miiki\\Desktop\\DATA_SCIENCE\\APRENDIZAJE_PYTHON\\venvs\\fundacio_a_p\\inputs\\Indicadors_mapa.csv')
 locations=pd.read_csv(DATA)
-
-# Ver las primeras filas para comprobar que se cargó bien
-locations.head(10)
 
 # Separamos la latitud y longitud en columnas diferentes.
 # Suponiendo que la columna se llama "coordenadas" y está en formato "lat, lon"
@@ -22,15 +19,32 @@ locations[['lon', 'lat']] = locations['Coordenades\n(longitud, latitud)'].str.sp
 locations['lat'] = locations['lat'].astype(float)
 locations['lon'] =locations['lon'].astype(float)
 
-locations['Xeringues'] = locations['Xeringues'].fillna(0)
-
+#Agrupamos por la columna de coordenadas para obtener la suma
+gr_locations =locations.groupby(['lon','lat','Tipo'],as_index=False)['Cantidad'].sum()
+gr_locations.head(10)
 
 
 # Datos con latitud/longitud y valores
 
-fig = px.scatter_map(locations, lat = 'lat', lon = 'lon', size = 'Xeringues',
-                        zoom = 4, map_style = 'open-street-map')
+fig = px.scatter_map(gr_locations,
+                      lat = 'lat', lon = 'lon',
+                        size = 'Cantidad',
+                        color= 'Tipo',
+                        zoom = 4,
+                        map_style = 'carto-positron',
+                        color_discrete_map={"Xeringues": "#3357FF", "RC:Injectat": "#FF5733"
+                        })
+
+fig.update_layout(
+    title="Mapa de xeringues i restes de consum",
+    title_x=0.5,  # Centrado del título
+    title_font=dict(size=24, color="black", family="Verdana"),
+    height=800,  # Altura personalizada
+    width=1200 # anchura personalizada
+)
+
                         
 fig.show()
+                        
 
-st.plotly_chart(fig)
+st.plotly_chart(fig,use_container_width=True)
